d32e5675732e54ecca64338b287b2dea7f163435
Alan <atomusiak6@gmail.com>
2020-04-26 03:13:13 +0000

    NOW?

---
+++ public/index.js
@@ -0,0 +1,140 @@
+socket = io('http://54.212.89.190:3000', {transports: ['websocket']});
+socket.on('message', function(data) {
+  console.log(data);
+});
+socket.on('name', function(data) {
+  // data is a parameter containing whatever data was sent
+});
+var movement = {
+  up: false,
+  down: false,
+  left: false,
+  right: false
+}
+
+document.addEventListener('keydown', function(event) {
+  switch (event.keyCode) {
+    case 65: // A
+      movement.left = true;
+      break;
+    case 87: // W
+      movement.up = true;
+      break;
+    case 68: // D
+      movement.right = true;
+      break;
+    case 83: // S
+      movement.down = true;
+      break;
+  }
+});
+
+document.addEventListener('keyup', function(event) {
+  switch (event.keyCode) {
+    case 65: // A
+      movement.left = false;
+      break;
+    case 87: // W
+      movement.up = false;
+      break;
+    case 68: // D
+      movement.right = false;
+      break;
+    case 83: // S
+      movement.down = false;
+      break;
+  }
+});
+
+socket.emit('new player');
+setInterval(function() {
+  socket.emit('movement', movement);
+}, 1000 / 60);
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var circles = [];
+var lines = [];
+
+CanvasRenderingContext2D.prototype.clear =
+  CanvasRenderingContext2D.prototype.clear || function (preserveTransform) {
+    if (preserveTransform) {
+      this.save();
+      this.setTransform(1, 0, 0, 1, 0, 0);
+    }
+    this.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    if (preserveTransform) {
+      this.restore();
+    }
+}
+
+function init(){
+    circles = [];
+    lines = [];
+    audios = [];
+    for (i= 0; i < 5; i++) {
+      for (j = 0; j < 5; j++) {
+        circles.push({
+          colour: '#FF0000',
+          x: 0 + 250 * i,
+          y: 0 + 250 * j,
+          radius: 10
+          }
+        );
+        lines.push( {
+          start_x: 0 + 250*i,
+          start_y: 0,
+          end_x: 250*i,
+          end_y: 1000,
+          }
+        );
+        lines.push( {
+          start_x: 0,
+          start_y: 0 + 250*j,
+          end_x: 1000,
+          end_y: 0 + 250*j,
+          }
+        );
+      }
+    }
+}
+
+function redrawCanvas(players) {
+    ctx.clear();
+    lines.forEach(lines =>  {
+      	ctx.beginPath();
+      	ctx.moveTo(lines.start_x,lines.start_y);
+    	ctx.lineTo(lines.end_x,lines.end_y)
+    	ctx.stroke();
+    })
+    circles.forEach(circle => {
+   	ctx.beginPath();
+    	ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI, false);
+    	ctx.fillStyle = circle.colour;
+    	ctx.fill();
+    });
+    for (var id in players) {
+    	var player = players[id];
+    	ctx.beginPath();
+    	ctx.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+    	ctx.fillStyle = '#00FF00'
+    	ctx.fill();
+  }
+
+  }
+
+socket.on('state', function(players) {
+  redrawCanvas(players);
+});
+
+socket.on('click',function(players) {
+	
+});
+
+canvas.addEventListener('click', function() {
+    var x = event.pageX,
+        y = event.pageY;
+  }, false);
+
+init();
+
