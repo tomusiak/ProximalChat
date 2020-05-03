3a88c108322dc35a40e30507e0c8da665bb6fd3a
Alan Tomusiak <atomusiak6@gmail.com>
2020-05-02 22:55:09 -0700

    idk what this is

--- public/index.js
+++ public/index.js
@@ -276,4 +276,61 @@ function pageReady() {
     }
 }
 
+function getUserMediaSuccess(stream) {
+    localStream = stream;
+    localVideo.src = window.URL.createObjectURL(stream);
+}
+
+function getUserMediaError(error) {
+    console.log(error);
+}
+
+function start(isCaller) {
+    peerConnection = new RTCPeerConnection(peerConnectionConfig);
+    peerConnection.onicecandidate = gotIceCandidate;
+    peerConnection.onaddstream = gotRemoteStream;
+    peerConnection.addStream(localStream);
+
+    if(isCaller) {
+        peerConnection.createOffer(gotDescription, createOfferError);
+    }
+}
+
+function gotDescription(description) {
+    console.log('got description');
+    peerConnection.setLocalDescription(description, function () {
+        serverConnection.send(JSON.stringify({'sdp': description}));
+    }, function() {console.log('set description error')});
+}
+
+function gotIceCandidate(event) {
+    if(event.candidate != null) {
+        serverConnection.send(JSON.stringify({'ice': event.candidate}));
+    }
+}
+
+function gotRemoteStream(event) {
+    console.log('got remote stream');
+    remoteVideo.src = window.URL.createObjectURL(event.stream);
+}
+
+function createOfferError(error) {
+    console.log(error);
+}
+
+function gotMessageFromServer(message) {
+    if(!peerConnection) start(false);
+
+    var signal = JSON.parse(message.data);
+    if(signal.sdp) {
+        peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp), function() {
+            if(signal.sdp.type == 'offer') {
+                peerConnection.createAnswer(gotDescription, createAnswerError);
+            }
+        });
+    } else if(signal.ice) {
+        peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice));
+    }
+}
+
 init();
