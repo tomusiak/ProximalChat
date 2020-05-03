3297baacdbbb9ccbfb25f83b0e0978b0a23d32f5
Alan Tomusiak <atomusiak6@gmail.com>
2020-05-02 22:49:46 -0700

    will even this work

--- public/index.js
+++ public/index.js
@@ -249,19 +249,4 @@ $(document).ready(function(){
   });
 });
 
-const webrtc = new SimpleWebRTC({
-  // the id/element dom element that will hold "our" video
-  localVideoEl: 'local_video',
-  // the id/element dom element that will hold remote videos
-  remoteVideosEl: 'remote_video',
-  // immediately ask for camera access
-  autoRequestMedia: true,
-});
-
-// We got access to local camera
-webrtc.on('localStream', () => {
-  localImageEl.hide();
-  localVideoEl.show();
-});
-
 init();
