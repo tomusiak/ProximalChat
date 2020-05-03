2fdf832be5f3479b87bbfd1c6eec894c059a2b0b
Alan Tomusiak <atomusiak6@gmail.com>
2020-05-02 22:24:40 -0700

    dumb hail mary

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
-socket = io('https://54.212.89.190:3000', {transports: ['websocket']});
-
+//socket = io('https://54.212.89.190:3000', {transports: ['websocket']});
+socket=io();
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var circles = [];
