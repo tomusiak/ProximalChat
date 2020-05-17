1c2f4b914428277c351989f024b4cf76b06429a9
Alan Tomusiak <atomusiak6@gmail.com>
2020-05-16 22:17:21 -0700

    logging

--- public/index.js
+++ public/index.js
@@ -28,6 +28,7 @@ const video_array = [
 const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]}
 const peerConnection = new RTCPeerConnection(configuration);
 peerConnection.onaddstream = event => {
+  socket.emit("log","streaming!");
   const remoteVideo = document.getElementById("video_5");
   remoteVideo.srcObject = event.stream;
 };
