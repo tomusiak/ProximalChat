283a9afbc58428e942c075a2665a2769e1c447b3
Alan Tomusiak <atomusiak6@gmail.com>
2020-05-17 01:12:25 -0700

    is it getting the right id?

--- public/index.js
+++ public/index.js
@@ -320,7 +320,7 @@ socket.on("watcher", data => {
         .createOffer()
         .then(sdp => peerConnection.setLocalDescription(sdp))
         .then(() => {
-          socket.emit("log","in starting");
+          socket.emit("log",id);
           socket.emit("offer", id, peerConnection.localDescription);
         });
       }
