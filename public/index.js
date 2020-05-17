d15a8aaae930a91a085a7bf5deee84715a90590d
Alan Tomusiak <atomusiak6@gmail.com>
2020-05-16 21:06:42 -0700

    why isnt it ever going to ontrack

--- public/index.js
+++ public/index.js
@@ -27,9 +27,9 @@ const video_array = [
 const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]}
 const peerConnection = new RTCPeerConnection(configuration);
 peerConnection.ontrack = function({ streams: [stream] }) {
+  socket.emit("log");
  const remoteVideo = document.getElementById("video_5");
  if (remoteVideo) {
-   socket.emit("log");
    remoteVideo.srcObject = stream;
  }
 };
