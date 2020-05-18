49a0bf4194ecfa24576ec49d54b7e2f3da684d98
Alan Tomusiak <atomusiak6@gmail.com>
2020-05-17 21:48:23 -0700

    try

--- public/index.js
+++ public/index.js
@@ -291,9 +291,7 @@ const config = {
 socket.on("watcher", data => {
   const caller = data.caller;
   var stream;
-  socket.emit("log",caller);
-  socket.emit("log",data);
-  socket.emit("log",users);
+  socket.emit("log",users.caller);
   local_video_slot = data.users.caller;
   navigator.mediaDevices.getUserMedia({audio: true, video: true})
   .then(mediaStream => {
