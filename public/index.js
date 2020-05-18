1a0eb60241896d9213c0a3ac9c5451f55c9ee3e0
Alan Tomusiak <atomusiak6@gmail.com>
2020-05-17 23:10:39 -0700

    hnm

--- public/index.js
+++ public/index.js
@@ -358,12 +358,13 @@ socket.on("offer", (callee, description, caller) => {
   var video = 0;
   for (vid in video_array) {
     if (video_occupancy[vid] = false) {
+      socket.emit("log",vid);
       video = document.getElementById(vid);
       video_occupancy[vid] = true;
     }
   }
   peerConnection.ontrack = event => {
-    socket.emit("log","in callee ontrack");
+    socket.emit("log",video);
     video.srcObject = event.streams[0];
   };
   peerConnection.onicecandidate = event => {
